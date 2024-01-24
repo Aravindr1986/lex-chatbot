93f5866b9b9fc1983d635ad9072f6dc983a86daa
Aravind R <aravind.ramachandran1@ey.com>
2024-01-24 09:20:34 -0500

    Added local env setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// src/app/app.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { AmplifyService } from 'aws-amplify-angular';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'lex-chatbot-app';
+export class AppComponent implements OnInit {
+  constructor(private amplifyService: AmplifyService) {}
+
+  ngOnInit() {
+    this.amplifyService.auth().currentAuthenticatedUser().then(user => {
+      console.log(user);
+    }).catch(err => console.log(err));
+  }
 }
